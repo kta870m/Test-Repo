ad71f5d8e4c6abc3f6962e0e1bbdabd0f388c0da
kta870m <kta87067@gmail.com>
2024-06-12 21:26:46 +0700

    update-css

---
+++ Bai1.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class Bai1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double Dong500 = 0, Dong200 = 0, Dong100 = 0, Dong50 = 0;
+        boolean flag = true;
+        int input = 0;
+
+        do{
+        System.out.print("Nhap so tien can rut: ");
+        while(!scanner.hasNextInt()){
+            System.out.println("Day khong phai la so vui long nhap lai");
+            scanner.next();
+            System.out.print("Nhap so tien can rut: ");
+        }
+        input = scanner.nextInt();
+
+            if(input <= 0){
+                System.out.println("So tien can rut ko hop le");
+                flag = false;
+            }else{
+                flag = true;
+            }
+        }while(!flag);
+
+
+        if (flag) {
+            System.out.println("Input nhap vao: " + input);
+            Dong500 = input / 500000;
+            input = input % 500000;
+
+            Dong200 = input / 200000;
+            input = input % 200000;
+
+            Dong100 = input / 100000;
+            input = input % 100000;
+
+            Dong50 = input / 50000;
+            input = input % 50000;
+
+            double total = Dong500 + Dong200 + Dong100 + Dong50;
+
+            System.out.println("So to 500000: " + (int) Dong500);
+            System.out.println("So to 200000: " + (int) Dong200);
+            System.out.println("So to 100000: " + (int) Dong100);
+            System.out.println("So to 50000: " + (int) Dong50);
+            System.out.println("Tong so to: " + (int) total);
+        }
+    }
+}
---
+++ Bai2.java
@@ -0,0 +1,81 @@
+import java.util.Scanner;
+
+
+public class Bai2 {
+    public static void main(String[] args){
+        double soTienLai = 0;
+        Scanner scanner = new Scanner(System.in);
+        int inputOption;
+        int input = 0;
+        boolean flag = true;
+        double result;
+
+
+
+        do{
+            System.out.print("Nhap so tien muon gui: ");
+            while(!scanner.hasNextInt()){
+                System.out.println("Day khong phai la so vui long nhap lai");
+                scanner.next();
+                System.out.print("Nhap so tien muon gui: ");
+            }
+            input = scanner.nextInt();
+
+            if(input <= 0){
+                System.out.print("So tien can rut ko hop le");
+                flag = false;
+            }else{
+                flag = true;
+            }
+        }while(!flag);
+
+
+        do{
+            System.out.print("Chon thang lai suat muon gui (6, 9, 12, 24, 36): ");
+            while(!scanner.hasNextInt()){
+                System.out.println("Day khong phai la so vui long nhap lai");
+                scanner.next();
+                System.out.print("Nhap so thang gui: ");
+            }
+            inputOption = scanner.nextInt();
+
+        switch (inputOption) {
+            case 6:
+                soTienLai = input * 5.1 / 100;
+                flag = true;
+                break;
+            case 9:
+                soTienLai = input * 5.5 / 100;
+                flag = true;
+                break;
+            case 12:
+                soTienLai = input * 6.4 / 100;
+                flag = true;
+                break;
+            case 24:
+            case 36:
+                soTienLai = input * 6.5 / 100;
+                flag = true;
+                break;
+            default:
+                System.out.println("Option khong hop le");
+                flag = false;
+                break;
+        }
+
+        }while(!flag);
+
+        result = input + soTienLai;
+
+        System.out.println("input: " + input);
+        System.out.println("Loai tien gui: VND");
+        System.out.println("Tien lai theo ky: " + (int)soTienLai);
+        System.out.println("Lai + Goc: " + (int)result);
+    }
+
+
+    }
+
+
+
+
